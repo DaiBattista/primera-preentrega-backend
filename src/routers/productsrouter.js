5a4807522b62acbb86ea7a2ce3083b740d4a01b1
Daiana Battista <dbattista.dg@gmail.com>
2023-11-01 09:35:45 -0300

    correccion preentrega

--- src/routers/productsrouter.js
+++ src/routers/productsrouter.js
@@ -4,11 +4,47 @@ const encoding = 'utf-8';
 const products = require('../products.json');
 const router = Router();
 
-router.get('/products', (req, res) => {
-    res.setHeader('Content-Type', 'application/json; charset=utf-8');
-    res.status(200).json(products);
-});
+// Ruta raíz GET /products
+router.route('/products')
+    .get((req, res) => {
+        const limit = req.query.limit;
+        const limitedProducts = limit ? products.slice(0, limit) : products;
 
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+        res.status(200).json(limitedProducts);
+    })
+    .post((req, res) => {
+        const { body } = req;
+        const { title, description, code, price, stock, category } = body;
+        const status = body.status !== undefined ? body.status : true;
+        const thumbnails = body.thumbnails || [];
+
+        // Validar campos obligatorios
+        if (!title || !description || !code || !price || !stock || !category) {
+            return res.status(400).json({
+                message: 'Se requieren campos esenciales: title, description, code, price, stock, category.',
+            });
+        }
+
+        const newProduct = {
+            id: uuidV4(),
+            title,
+            description,
+            code,
+            price,
+            status,
+            stock,
+            category,
+            thumbnails,
+        };
+
+        products.push(newProduct);
+
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+        res.status(201).json(newProduct);
+    });
+
+// Ruta GET /products/:pid
 router.get('/products/:pid', (req, res) => {
     const pid = req.params.pid;
     const product = products.find((product) => product.id === pid);
@@ -21,7 +57,8 @@ router.get('/products/:pid', (req, res) => {
     res.status(200).json(product);
 });
 
-router.put('/:pid', (req, res) => {
+// Ruta PUT /products/:pid
+router.put('/products/:pid', (req, res) => {
     const pid = req.params.pid;
     const product = products.find((product) => product.id === pid);
 
@@ -39,48 +76,24 @@ router.put('/:pid', (req, res) => {
     res.status(200).json(product);
 });
 
-router.post('/products', (req, res) => {
-    const { body } = req;
-    const { title, description, code, price, stock, category } = body;
-    const status = body.status !== undefined ? body.status : true;
-    const thumbnails = body.thumbnails || [];
-
-    if (!title || !description || !code || !price || !stock || !category) {
-        return res.status(400).json({
-            message: 'Se requieren campos esenciales: title, description, code, price, stock, category.',
-        });
-    }
-
-    const newProduct = {
-        id: uuidV4(),
-        title,
-        description,
-        code,
-        price,
-        status,
-        stock,
-        category,
-        thumbnails,
-    };
-
-    products.push(newProduct);
-
-    res.setHeader('Content-Type', 'application/json; charset=utf-8');
-    res.status(201).json(newProduct);
-});
-
-router.delete('/:pid', (req, res) => {
+// Ruta DELETE /products/:pid
+router.delete('/products/:pid', (req, res) => {
     const pid = req.params.pid;
-    const product = products.find((product) => product.id === pid);
+    const productIndex = products.findIndex((product) => product.id === pid);
 
-    if (!product) {
+    if (productIndex === -1) {
         return res.status(404).json({ message: 'Producto no encontrado' });
     }
 
-    products.splice(products.indexOf(product), 1);
+    products.splice(productIndex, 1);
 
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
     res.status(200).json({ message: 'Producto eliminado' });
 });
 
 module.exports = router;
+
+
+
+
+
